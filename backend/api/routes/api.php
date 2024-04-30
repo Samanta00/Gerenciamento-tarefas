1ffc32eaf600c69ddfe5089b7de9c9860b2458e7
Ellen Samanta <ellen.samanta@outlook.com>
2024-04-30 19:33:12 -0300

    feat: adicionando rotas novas

--- backend/api/routes/api.php
+++ backend/api/routes/api.php
@@ -2,18 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TaskController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+//rotas da api
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'api'], function ($router){
+    // Route::post('login', [AuthController::class, 'login']);
+    // Route::post('users', [UserController::class, 'index']);
+
+    Route::get('task/view/{id}', [TaskController::class, 'get']);
+    Route::get('task', [TaskController::class, 'getList']);
+    Route::post('task', [TaskController::class, 'store']);
+    Route::put('task/update/{id}', [TaskController::class, 'update']);
+    Route::delete('task/delete/{id}', [TaskController::class, 'destroy']);
+    Route::post('task/search/{lembrete}', [TaskController::class, 'filterbyLembrete']);
+    Route::post('task/pendency/{pendency}', [TaskController::class, 'filterBySituacao']);
+    
+
 });
